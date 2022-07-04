cd684f6b481b0d911654dca8fbea5264bc57f11a
fclskn <108349810+fclskn@users.noreply.github.com>
2022-07-04 20:09:51 +0300

    Add files via upload

--- FileProcessing.js
+++ FileProcessing.js
@@ -1,1 +1,24 @@
-"# FileProcessingInNODEJS" 
+//var fs = require('fs');
+
+//fs.appendFile('bos.txt','burayi bos.js den bos.txt yaziyorum1', function(err){//yazma fonksiyonu dosya yoksa dosya olusturur
+ //   if (err) throw err;
+   // console.log('Saved!1')});
+
+
+/*fs.open('bos.txt','w',function(err,file){
+    if (err) throw err;
+})*/
+
+//fs.open('mynewfile2.txt', 'w', //Dosya acar
+  //  console.log('Saved!2'));
+
+//fs.writeFile('output.txt','write file tarafindan yaziliyor',(err,)=>{ if(err) throw err;
+//console.log("Save 3")});
+
+//fs.appendFile('output.txt','buranin outputun sonuna gelmesi gerekiyor',(err)=>{if (err) throw err;
+//console.log("Save 4")});
+
+//fs.unlink('output.txt',(err)=>{if (err) throw err; console.log("Save 5")}); TEHLIKELI COP KUTUSUNA ATMIYOR..
+
+//fs.rename('output.txt','output2.txt',(err)=>{if (err) throw err; console.log("Save 6")}) output-->output2
+
